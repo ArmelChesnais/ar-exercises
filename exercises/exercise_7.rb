a1abdc645931a2d51151bc8496eb6163c38da9ff
Armel Chesnais <armel.chesnais@gmail.com>
2017-06-07 20:07:20 +0000

    completed ex7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,17 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please enter a store name:"
+print "> "
+@store_name = gets.chomp
+
+@new_store = Store.create(name: @store_name)
+
+unless @new_store.valid?
+  puts "======="
+  puts "Errors:"
+  @new_store.errors.full_messages.each do |message|
+    puts message
+  end
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
+  validate :mens_or_womens_apparel
+
+  def mens_or_womens_apparel
+    unless mens_apparel.present? || womens_apparel.present?
+      errors.add(:men_or_womens_apparel, "must be true")
+    end
+  end
 end
