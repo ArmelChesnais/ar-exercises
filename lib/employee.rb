313d07b4769f98d83a59157ccd49bcf50bf7a988
Armel Chesnais <armel.chesnais@gmail.com>
2017-06-07 20:51:05 +0000

    completed ex8 (stretch)

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,4 +4,15 @@ class Employee < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :store
   validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200, only_integer: true }
   validates_associated :store
+
+  after_create :create_password
+  # before_save :create_password  # => would re-write a new password each time a change was made and saved to the entry
+
+  private
+
+  def create_password
+    self.password = 8.times.map { [*'0'..'9', *'a'..'z', *'A'..'Z'].sample }.join
+    self.save
+  end
+
 end
